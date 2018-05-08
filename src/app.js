38a85f2fe19505b34e9d4cf74d98b40e8ddf523b
Shilei Ma <604237073@qq.com>
2018-05-08 23:22:58 +0800

    实践 jsx 语法，修复 this 指向

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const element = (<div>
-    <div>Hello World!</div>
-    <h1>这是 h1</h1>
-</div>)
+class Component extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name:'Loading',
+            age : 20
+        }
+    }
+    handleClick(){
+        this.setState({
+            age: this.state.age + 1
+        })
+    }
+    render(){
+        return (
+            <div>
+                <h1>Hello,I am {this.state.name}</h1>
+                <p>I am {this.state.age} years old</p>
+                <button onClick={()=>{this.handleClick()}}>加一岁</button>
+            </div>
+        )
+    }
+}
                 
-ReactDom.render(element,document.getElementById('root'))
+ReactDom.render(<Component/>,document.getElementById('root'))
