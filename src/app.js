d70ef549140fc8ebd7a35da854449f39ad6e8fda
Loading <shileima@hotmail.com>
2018-05-15 16:08:33 +0800

    深入webpack常用配置loader,dev-server,scripts等

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import DidMountFor from './test/didMoutFor';
+
+import './index.css';
+import './index.scss';
+import 'font-awesome/css/font-awesome.min.css'
+
+class Wrapper extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+               <h1>Hello</h1>
+               <i className="fa fa-address-book"></i>
+            </div>
+        )
+    }
+}
+
                 
-ReactDom.render(<DidMountFor/>,document.getElementById('root'))
+ReactDom.render(<Wrapper />,document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 const path              = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack           = require('webpack')
 
 module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        publicPath: '/dist/',
+        filename: 'js/app.js'
     },
     plugins: [
+        //处理html文件
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        //独立css文件
+        new ExtractTextPlugin('css/[name].css'),
+        //提取公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'common',
+            filename: 'js/base.js'
         })
     ],
     devServer: {
-        contentBase: './dist'
+        port:8086
     },
     module: {
         rules: [
@@ -26,6 +37,45 @@ module.exports = {
                     presets: ['env','react']
                 }
             }
+          },
+          {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+              })
+          },
+          {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              //如果需要，可以在 sass-loader 之前将 resolve-url-loader 链接进来
+              use: ['css-loader', 'sass-loader']
+            })
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 8192,
+                  name: 'resource/[name].[ext]'
+                }
+              }
+            ]
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: 'resource/[name].[ext]'
+                    }
+                }
+            ]
+            
           }
         ]
       }
